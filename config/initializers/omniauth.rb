b022a1f53ab0323b7fdd1c71cf78c0f1cb6957f5
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-05-11 18:05:49 -0500

    fixed image

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           :scope => 'email', :display => 'popup', :secure_image_url => true, :image_size => 'normal'
+           :scope => 'email', :display => 'popup', :secure_image_url => true
 end
